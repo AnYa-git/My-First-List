7e5b83e45f6dc620a1ab4cf5a9c32c4a53a55d32
Anya <aynabenadda16@gmail.com>
2025-08-10 21:05:25 +0100

    ajout de commentaire javascript

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let buttonPlus = document.getElementById("btn+");
 let buttonMinus = document.getElementById("btn-");
 let container = document.getElementById("div-container");
 
+// CREATION D'UNE FONCTION QUI PERMET D'AJOUTER DES DIV
 function createCard() {
   let box = document.createElement("div");
   box.classList.add("custom-div");
   container.append(box);
 }
 
+// AJOUT DE L'EVENEMENT CLICK SUR LE BOUTON + POUR AJOUTER UNE DIV
 buttonPlus.addEventListener("click", createCard);
 
+// AJOUT DE L'EVENEMENT CLICK SUR LE BOUTON - POUR SUPPRIMER LA DERNIERE DIV
 buttonMinus.addEventListener("click", () => {
   let lastDiv = container.querySelector(".custom-div:last-of-type");
   if (lastDiv) {
