ba9cccb7d0080917d35d05abfafe44d9115295a0
Anya <aynabenadda16@gmail.com>
2025-08-10 21:53:59 +0100

    ajouter la valeur de l'input a la div quand on click sur le bouton +

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let buttonPlus = document.getElementById("btn+");
 let buttonMinus = document.getElementById("btn-");
 let container = document.getElementById("div-container");
+let input = document.getElementById("text-input");
 
 // CREATION D'UNE FONCTION QUI PERMET D'AJOUTER DES DIV
 function createCard() {
   let box = document.createElement("div");
   box.classList.add("custom-div");
+
+  // ON MET LE CONTENU DE L'INPUT
+  box.textContent = input.value;
+
   container.append(box);
+
+  // VIDER L'INPUT APRES L'AJOUT
+  input.value = "";
 }
 
 // AJOUT DE L'EVENEMENT CLICK SUR LE BOUTON + POUR AJOUTER UNE DIV
